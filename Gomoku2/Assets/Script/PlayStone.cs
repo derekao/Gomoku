df0aa8e168d254e301e361d486b92e6f6570fb1f
Sebastien MARTIN <semartin@e3r3p5.42.fr>
2018-03-13 15:38:52 +0100

    play

--- Gomoku2/Assets/Script/PlayStone.cs
+++ Gomoku2/Assets/Script/PlayStone.cs
@@ -92,27 +92,6 @@ public class PlayStone : MonoBehaviour {
 		}
 		else {
 			OnWhitePlay();
-			GameStatus game;
-			game.Board = new int[GameManager.Instance.iHeightBoard * GameManager.Instance.iWidthBoard];
-			game.bPlayerOneTurn = GameManager.Instance.currentState.bPlayerOneTurn;
-			game.HasWon = GameManager.Instance.currentState.hasWon;
-			game.WhiteScore = GameManager.Instance.currentState.WhiteScore;
-			game.BlackScore = GameManager.Instance.currentState.BlackScore;
-			game.WinY = GameManager.Instance.currentState.winY;
-			game.WinX = GameManager.Instance.currentState.winX;
-			for (int i = 0; i < GameManager.Instance.iHeightBoard; i++)
-			{
-				for (int j = 0; j < GameManager.Instance.iWidthBoard; j++)
-				{
-					game.Board[i * GameManager.Instance.iWidthBoard + j] = GameManager.Instance.currentState.Board[i,j];
-				}
-			}
-			CoordIA test = IAPlay(game);
-			Debug.Log("y = " + test.y + " et x = " +  test.x);
-			int id = test.x + test.y * GameManager.Instance.iWidthBoard + 1;
-			GameObject box = GameObject.Find("Stone (" + id + ")");
-			PlayStone Move = box.GetComponent<PlayStone>();
-			Move.OnBlackPlay();
 
 		}
 
@@ -183,6 +162,27 @@ public class PlayStone : MonoBehaviour {
 			}
 			checkBoardState(y, x, SomethingEaten);
 			Rules.DisplayBoard();
+			GameStatus game;
+			game.Board = new int[GameManager.Instance.iHeightBoard * GameManager.Instance.iWidthBoard];
+			game.bPlayerOneTurn = GameManager.Instance.currentState.bPlayerOneTurn;
+			game.HasWon = GameManager.Instance.currentState.hasWon;
+			game.WhiteScore = GameManager.Instance.currentState.WhiteScore;
+			game.BlackScore = GameManager.Instance.currentState.BlackScore;
+			game.WinY = GameManager.Instance.currentState.winY;
+			game.WinX = GameManager.Instance.currentState.winX;
+			for (int i = 0; i < GameManager.Instance.iHeightBoard; i++)
+			{
+				for (int j = 0; j < GameManager.Instance.iWidthBoard; j++)
+				{
+					game.Board[i * GameManager.Instance.iWidthBoard + j] = GameManager.Instance.currentState.Board[i,j];
+				}
+			}
+			CoordIA test = IAPlay(game);
+			Debug.Log("y = " + test.y + " et x = " +  test.x);
+			int id = test.x + test.y * GameManager.Instance.iWidthBoard + 1;
+			GameObject box = GameObject.Find("Stone (" + id + ")");
+			PlayStone Move = box.GetComponent<PlayStone>();
+			Move.OnBlackPlay();
 		}
 
 	}
